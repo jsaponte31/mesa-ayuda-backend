7e64676ef0f7e4e4bb314bd44e19e647bcbd9629
“jhojanapo11“ <jsaponte@uts.edu.co>
2024-10-20 00:20:20 -0500

    cambio

--- routes/api.php
+++ routes/api.php
@@ -6,10 +6,6 @@ use App\Http\Controllers\userController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/user', function (Request $request) {
-//     return $request->user();
-// })->middleware('auth:sanctum');
-
 Route::post('/login', [loginController::class, 'login']);
 Route::post('/register', [loginController::class, 'register']);
 Route::middleware('auth:sanctum')->group(function () {
